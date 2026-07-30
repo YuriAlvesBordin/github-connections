d4bfd468a0c6574a05d926ad782f18769891a3d8
Yuri Alves Bordin <130792188+YuriAlvesBordin@users.noreply.github.com>
2026-07-30 11:25:59 -0300

    feat: create js/graph.js with basic graph structure

---
+++ js/graph.js
@@ -0,0 +1,13 @@
+// Estrutura de dados do grafo
+export const graph = {
+    nodes: [],
+    edges: []
+};
+
+export function addNode(id, x, y) {
+    graph.nodes.push({ id, x, y, vx: 0, vy: 0 });
+}
+
+export function addEdge(source, target) {
+    graph.edges.push({ source, target });
+}
