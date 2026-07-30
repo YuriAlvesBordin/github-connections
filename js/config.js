325fce38b3402ba9a7a6197e56692db1f3966aac
Yuri Alves Bordin <130792188+YuriAlvesBordin@users.noreply.github.com>
2026-07-30 11:21:14 -0300

    feat: create js/config.js with app constants

---
+++ js/config.js
@@ -0,0 +1,14 @@
+// Configuraçªıes do projeto
+export const CONFIG = {
+    // Cores
+    colors: {
+        background: '#0d1117',
+        text: '#c9d1d9',
+        node: '#58a6ff',
+        edge: '#30363d'
+    },
+    
+    // Tamanhos
+    nodeRadius: 8,
+    edgeWidth: 1
+};
