125e082a1a76f5a0f01d9fbf4e055ab4367d9dbb
Yuri Alves Bordin <130792188+YuriAlvesBordin@users.noreply.github.com>
2026-07-30 11:19:30 -0300

    feat: create js/main.js with canvas setup

---
+++ js/main.js
@@ -0,0 +1,11 @@
+// Setup inicial do canvas
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+
+function resize() {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+}
+
+resize();
+window.addEventListener('resize', resize);
